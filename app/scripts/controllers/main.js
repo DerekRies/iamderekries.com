3783c3c665701b9a21296660305538a1b2cf4d63
Derek Ries <influenztial@gmail.com>
2013-05-09 12:28:48 -0700

    Finished rotator components, now animateable and customizable. Changed page transition to horizontal fade

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,39 +8,38 @@ angular.module('portfolioApp')
       'AngularJS',
       'Karma'
     ];
-
     $scope.likesOne = [
-      'Physics',
-      'Astronomy',
-      'Skateboarding',
-      'Programming',
-      'Javascript',
-      'Cinder',
-      'C++',
-      'HTML5',
-      'Python',
-      'Angular.js',
-      'Yeoman',
-      'Bootstrap',
-      'Hacking',
-      'Wireshark',
-      'nmap',
-      'Google App Engine',
-      'Khan Academy',
-      'Coursera',
-      'Udacity',
-      'Engineering',
-      'Automation'
+      {text: 'Physics', width: 120},
+      {text:'Astronomy', width:170},
+      {text:'Skateboarding', width:190},
+      {text:'Programming', width:190},
+      {text:'Javascript', width:160},
+      {text:'Cinder', width:110},
+      {text:'C++', width:90},
+      {text:'Math', width:100},
+      {text:'HTML5', width:120},
+      {text:'Python', width:120},
+      {text:'Angular.js', width:150},
+      {text:'Yeoman', width:135},
+      {text:'Bootstrap', width:150},
+      {text:'Hacking', width:130},
+      {text:'nmap', width:100},
+      {text:'Wireshark', width:160},
+      {text:'App Engine', width:175},
+      {text:'Coursera', width:130},
+      {text:'Udacity', width:120},
+      {text:'Engineering', width:160},
+      {text:'Automation', width:160}
     ];
 
     $scope.likesTwo = [
-      'Web Apps',
-      'Data Visualizations',
-      'Games',
-      'Simulations',
-      'Native Apps',
-      'Tools',
-      'Bots'
+      {text: 'Web Apps', width: 150},
+      {text: 'Simulations', width:250},
+      {text: 'Data Visualizations', width: 250},
+      {text: 'Native Apps', width: 220},
+      {text: 'Games', width: 120},
+      {text: 'Tools', width: 100},
+      {text: 'Bots', width: 100}
     ];
 
     $scope.something = function (thing) {
--- app/scripts/directives/rotator.js
+++ app/scripts/directives/rotator.js
@@ -8,18 +8,40 @@ angular.module('portfolioApp')
       scope: {
         items: '=',
         duration: '@',
-        fudge: '@'
+        fudge: '@',
+        width: '@'
       },
-      template: '<div class="rotator-component">{{items[r]}}</div>',
-      // template: '<div class="rotator-parent"><div class="rotator-component" ng-repeat="item in items">{{item}}</div></div>',
-      // template: '<div class="rotator-parent"><div class="rotator-component"><div>{{items[r]}}</div></div>'+
-      //           '<div class="rotator-component"><div>{{items[r + 1]}}</div></div></div>',
+      template: '<div class="rotator-parent">' +
+                '<div class="rotator-component top">Test</div>' +
+                '<div class="rotator-component mid">Test</div>' +
+                '<div class="rotator-component btm">Test</div>' +
+                '</div>',
       link: function postLink(scope, element, attrs) {
-        scope.r = 0;
-        var rotate = function(){
+        var top,
+            mid,
+            btm,
+            prev,
+            el = $(element);
+        scope.r = 3;
+        console.log(scope.width);
+        $(element.children()[0]).html(scope.items[0].text);
+        $(element.children()[1]).html(scope.items[1].text);
+        $(element.children()[2]).html(scope.items[2].text);
+        element.css('width', scope.width + 'px');
 
+        var rotate = function(){
             $timeout(function(){
-              scope.r = (scope.r >= scope.items.length - 2) ? 0 : scope.r + 1;
+              top = el.find('.top');
+              mid = el.find('.mid');
+              btm = el.find('.btm');
+              top.removeClass('top').addClass('btm');
+              mid.removeClass('mid').addClass('top');
+              btm.removeClass('btm').addClass('mid');
+              top.html(scope.items[scope.r].text);
+              prev =  (scope.r - 1) >= 0 ? scope.r - 1 : scope.items.length - 1;
+              console.log(scope.items[prev]);
+              element.css('width', scope.items[prev].width + 'px');
+              scope.r = (scope.r >= scope.items.length - 1) ? 0 : scope.r + 1;
               rotate();
             }, scope.duration);
           };
